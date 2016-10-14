7aa15d465c03961d2fdbd921694fa8e23110a042
csillag <csillag@harmacolindor.com>
2016-10-14 18:27:14 +0200

    Add helper function for generating the new state in the reducer.
    This way, we can enforce type safety ad the changes.

--- app/client/data/reducers.ts
+++ app/client/data/reducers.ts
@@ -1,11 +1,18 @@
 // The file containes Redux reducer function
-import { AppState } from './State';
+import { PartialAppState, AppState } from './State';
 import { Action,
          EDIT_URL, SUBMIT_URL, URL_ERROR,
          LOAD, LOAD_FAIL, LOAD_SUCCESS,
          PARSING_STARTED, PARSE_ERROR, FEED_PARSED
 } from './actions';
 
+// Helper funcion for generating the next state, by applying a set of
+// changes to the current state.
+function applyChanges(state:AppState, changes:PartialAppState):AppState {
+    return Object.assign({}, state, changes);
+}
+
+// This is the reducer function
 export function getNextState(state:AppState, action:Action):AppState {
     if (!state) {
         const result:AppState = {
@@ -23,48 +30,48 @@ export function getNextState(state:AppState, action:Action):AppState {
 //    console.log("Action:", action);
     switch (action.type) {
     case EDIT_URL:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             url: action.url,
             urlErrorMessage: "",
         });
     case SUBMIT_URL:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             shouldFetch: true,
         });
     case URL_ERROR:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             shouldFetch: false,
             urlErrorMessage: action.error,
         });
     case LOAD:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             shouldFetch: false,
             fetching: true,
             feed: null,
         });
     case LOAD_FAIL:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             fetching: false,
             urlErrorMessage: action.error.message || action.error.data,
         });
     case LOAD_SUCCESS:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             fetching: false,
             incomingResponse: action.payload.data,
             shouldParse: true,
         });
     case PARSING_STARTED:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             shouldParse: false,
             parsing: true,
         });
     case PARSE_ERROR:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             parsing: false,
             urlErrorMessage: action.error,
         });
     case FEED_PARSED:
-        return Object.assign({}, state, {
+        return applyChanges(state, {
             parsing: false,
             feed: action.results,
         });
--- app/client/data/state.ts
+++ app/client/data/state.ts
@@ -1,7 +1,18 @@
 
 import { AtomFeedInfo } from '../logic/atom';
 
-export interface AppState {
+export interface PartialAppState {
+    url?:string;
+    urlErrorMessage?:string;
+    shouldFetch?: boolean; // To trigger loading
+    fetching?: boolean;
+    incomingResponse?: string;
+    shouldParse?: boolean; // To trigger parsing
+    parsing?: boolean;
+    feed?: AtomFeedInfo;
+}
+
+export interface AppState extends PartialAppState {
     url:string;
     urlErrorMessage:string;
     shouldFetch: boolean; // To trigger loading
