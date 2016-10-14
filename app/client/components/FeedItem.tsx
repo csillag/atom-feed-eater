6597cf5877b22fec0560c8e79f4b6a12ec6afd5e
csillag <csillag@harmacolindor.com>
2016-10-14 14:44:37 +0200

    Make FeedItem a Stateless Function

--- app/client/components/FeedItem.tsx
+++ app/client/components/FeedItem.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { Component } from 'react';
-import * as classNames from 'classnames';
 
 import { Article } from '../logic/atom';
 
@@ -8,40 +6,37 @@ export interface FeedItemProps {
     item: Article;
 }
 
-export class FeedItem extends React.Component<FeedItemProps, {}> {
-
-    private renderImage(url:string) {
-        if (!url) {
-            return null;
-        }
-        return (<div><img
+const image = (url:string) => {
+    if (!url) {
+        return null;
+    }
+    return (<div><img
             className="atom-thumbnail"
             src={url}
-        /></div>)
-    }
+            /></div>)
+}
 
-    private renderBody(item:Article) {
-        if (item.description) {
-            return (<div className="atom-description" dangerouslySetInnerHTML={{__html: item.description}} />);
-        } else {
-            return (<blockquote className="atom-summary">{ item.summary }</blockquote>);
-        }
+const body = (item:Article) => {
+    if (item.description) {
+        return (<div className="atom-description" dangerouslySetInnerHTML={{__html: item.description}} />);
+    } else {
+        return (<blockquote className="atom-summary">{ item.summary }</blockquote>);
     }
+}
 
-    render() {
-        const item = this.props.item;
-        const date = item.date.toLocaleString();
-        return (<div className="atom-article">
-            <div className="atom-title">
-                <a target="_blank" href={item.link}>{ item.title }</a>
-            </div>
-            <div className="atom-content">
-                <span className="atom-author">{ item.author }</span>
-                <span>&nbsp;|&nbsp;</span>
-                <span className="atom-date">{ date }</span>
-                { this.renderImage(item.image) }
-                { this.renderBody(item) }
-            </div>
-        </div>);
-    }
+export const FeedItem = (props:FeedItemProps) => {
+    const item = props.item;
+    const date = item.date.toLocaleString();
+    return (<div className="atom-article">
+        <div className="atom-title">
+            <a target="_blank" href={item.link}>{ item.title }</a>
+        </div>
+        <div className="atom-content">
+            <span className="atom-author">{ item.author }</span>
+            <span>&nbsp;|&nbsp;</span>
+            <span className="atom-date">{ date }</span>
+            { image(item.image) }
+            { body(item) }
+        </div>
+    </div>);
 }
